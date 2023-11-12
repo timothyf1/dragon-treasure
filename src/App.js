9bd3b676372bfa5d5f16e11b9d930304668d91ae
Timothy Standen <timothyf1@live.co.uk>
2023-11-12 17:01:43 +0000

    Postion of objects now only stored in the state and board created based on those positions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
@@ -6,20 +5,56 @@ function Cell({ value }) {
   return <div className='board-cell'>{ value }</div>;
 }
 
-function Board() {
-  const [boardCells, setBoardCells] = useState(Array(5).fill(Array(5).fill(1)));
-
+function Board({ boardCells }) {
   return (
     <div className='board'>
       {boardCells.map((row, i) => (
         <div className='board-row' key={"row-"+i}>
-          {row.map((cell, j) => (<Cell value={cell} key={"row-"+i+"col"+j} />))}
+          {row.map((cell, j) => (<Cell value={cell} key={"r"+i+"c"+j} />))}
         </div>
       ))}
     </div>
   );
 }
 
+function Game() {
+  const [playerPosition, setPlayerPosition] = useState([0, 0]);
+  const [trapPosition, setTrapPosition] = useState([2, 1]);
+  const [dragonPosition, setDragonPosition] = useState([3, 3]);
+  const [treasurePosition, setTreasurePosition] = useState([4, 4]);
+
+  let board = createBoardCells(playerPosition, trapPosition, null, treasurePosition);
+
+  return <Board boardCells={board} />;
+}
+
 export default function App() {
-  return <Board />;
+  return <Game />;
+}
+
+/**
+ * Function to create the board based upon the current postions
+ * @param {*} playerPosition
+ * @param {*} trapPosition
+ * @param {*} dragonPosition
+ * @param {*} treasurePosition
+ * @returns A array which where each element is array for each row of the board.
+ */
+function createBoardCells(playerPosition, trapPosition, dragonPosition, treasurePosition) {
+  let boardCells = Array.from(Array(5), () => new Array(5).fill(null));
+
+  if (playerPosition) {
+    boardCells[playerPosition[0]][playerPosition[1]] = "P";
+  }
+  if (trapPosition) {
+    boardCells[trapPosition[0]][trapPosition[1]] = "T";
+  }
+  if (dragonPosition) {
+    boardCells[dragonPosition[0]][dragonPosition[1]] = "D";
+  }
+  if (treasurePosition) {
+    boardCells[treasurePosition[0]][treasurePosition[1]] = "X";
+  }
+
+  return boardCells;
 }
