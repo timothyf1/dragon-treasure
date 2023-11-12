99fe3c44885720ad59393380ad8d503e47e00cda
Timothy Standen <timothyf1@live.co.uk>
2023-11-12 15:40:53 +0000

    Basic board with some css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Cell({ value }) {
+  return <div className='board-cell'>{ value }</div>;
+}
+
+function Board() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='board'>
+      <div className='board-row'>
+        <Cell value={"P"} />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+      </div>
+      <div className='board-row'>
+        <Cell value={2} />
+        <Cell />
+        <Cell value={"T"} />
+        <Cell />
+        <Cell />
+      </div>
+      <div className='board-row'>
+        <Cell />
+        <Cell value={4} />
+        <Cell />
+        <Cell />
+        <Cell />
+      </div>
+      <div className='board-row'>
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell value={"L"} />
+        <Cell />
+      </div>
+      <div className='board-row'>
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell value={4} />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  return <Board />;
+}
